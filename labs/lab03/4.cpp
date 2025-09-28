73f8e2e553ab0efdfe14f40ed40de2df3e87dcdc
Daulet <dauleterlan07@gmail.com>
2025-09-28 13:41:06 +0500

    upd

---
+++ labs/lab03/4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() { 
+
+    double P = 3.14;
+    cout << P << endl;
+    cout << M_PI << endl;
+    printf("%.9f %.9f", P, M_PI);
+    return 0;
+}
+
