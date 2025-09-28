6533a895ac25b07e1e713e9c4e5dbe6f46c0be67
Daulet <dauleterlan07@gmail.com>
2025-09-28 13:59:56 +0500

    3_4.cpp

---
+++ labs/lab03/3_4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double x , y
+    cin >> x >> y >>
+    double A = 2*cos*(x-M_PI/6);
+    double B = 1/2 + (pow(sin(y),2));
+    double resA = A/B;
+    double A2 = atan(1/z);
+    double resB = cos(A2) * cos(A2);
+    cout << resA << " " << resB;
+    return 0;
+    c
+}   
--- labs/lab03/4.cpp
+++ labs/lab03/4.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() { 
 
     double P = 3.14;
+    
     cout << P << endl;
     cout << M_PI << endl;
     printf("%.9f %.9f", P, M_PI);
